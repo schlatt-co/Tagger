9462e61367ed719ec1ce031310a583f942dc9548
JRoy <joshroy126@gmail.com>
2020-01-29 16:39:37 -0500

    Revert "Close connections when we're done with them"
    This reverts commit 19d8fcba

--- src/main/java/io/github/jroy/tagger/sql/DatabaseManager.java
+++ src/main/java/io/github/jroy/tagger/sql/DatabaseManager.java
@@ -2,7 +2,6 @@ package io.github.jroy.tagger.sql;
 
 import io.github.jroy.tagger.events.TaggerTagUpdateEvent;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -46,14 +45,12 @@ public class DatabaseManager implements Listener {
     }
     plugin.getLogger().info("Connected!");
     plugin.getLogger().info("Initializing tables...");
-    Connection connection = getConnection();
-    connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `tags` ( `id` INT(15) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `price` INT(255) NOT NULL DEFAULT '0' , `text` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+    getConnection().createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `tags` ( `id` INT(15) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `price` INT(255) NOT NULL DEFAULT '0' , `text` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
     if (!isTag("none")) {
       createTag("none", "", 0);
     }
-    connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `usertags` ( `id` INT(15) NOT NULL AUTO_INCREMENT , `tagname` VARCHAR(255) NOT NULL , `uuid` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-    connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `activetags` ( `id` INT(15) NOT NULL AUTO_INCREMENT , `uuid` VARCHAR(255) NOT NULL , `tagname` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-    closeConnections(connection, null);
+    getConnection().createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `usertags` ( `id` INT(15) NOT NULL AUTO_INCREMENT , `tagname` VARCHAR(255) NOT NULL , `uuid` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+    getConnection().createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `activetags` ( `id` INT(15) NOT NULL AUTO_INCREMENT , `uuid` VARCHAR(255) NOT NULL , `tagname` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
     plugin.getLogger().info("Initialized tables!");
     plugin.getLogger().info("Processing tags...");
     cachedTags = getTags();
@@ -100,48 +97,39 @@ public class DatabaseManager implements Listener {
   }
 
   public void setPrice(Tag tag, int newPrice) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("UPDATE `tags` SET price = ? WHERE `name` = ?;");
+    PreparedStatement statement = getConnection().prepareStatement("UPDATE `tags` SET price = ? WHERE `name` = ?;");
     statement.setInt(1, newPrice);
     statement.setString(2, tag.getName());
     statement.executeUpdate();
     syncCache();
-    closeConnections(connection, statement);
   }
 
   public void setText(Tag tag, String text) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("UPDATE `tags` SET `text` = ? WHERE `name` = ?;");
+    PreparedStatement statement = getConnection().prepareStatement("UPDATE `tags` SET `text` = ? WHERE `name` = ?;");
     statement.setString(1, text);
     statement.setString(2, tag.getName());
     statement.executeUpdate();
     syncCache();
-    closeConnections(connection, statement);
   }
 
   public void awardTag(UUID uuid, Tag tag) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("INSERT INTO `usertags` (uuid, tagname) VALUES (?, ?);");
+    PreparedStatement statement = getConnection().prepareStatement("INSERT INTO `usertags` (uuid, tagname) VALUES (?, ?);");
     statement.setString(1, uuid.toString());
     statement.setString(2, tag.getName());
     statement.executeUpdate();
     ownedTags.get(uuid).add(tag);
-    closeConnections(connection, statement);
   }
 
   public void revokeTag(UUID uuid, Tag tag) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("DELETE FROM `usertags` WHERE uuid = ? AND tagname = ?");
+    PreparedStatement statement = getConnection().prepareStatement("DELETE FROM `usertags` WHERE uuid = ? AND tagname = ?");
     statement.setString(1, uuid.toString());
     statement.setString(2, tag.getName());
     statement.executeUpdate();
     ownedTags.get(uuid).remove(tag);
-    closeConnections(connection, statement);
   }
 
   private List<Tag> getUserTags(UUID uuid) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("SELECT  * FROM `usertags` WHERE uuid = ?;");
+    PreparedStatement statement = getConnection().prepareStatement("SELECT  * FROM `usertags` WHERE uuid = ?;");
     statement.setString(1, uuid.toString());
     ResultSet set = statement.executeQuery();
     List<Tag> tags = new ArrayList<>();
@@ -149,8 +137,6 @@ public class DatabaseManager implements Listener {
     while (set.next()) {
       tags.add(cachedTags.get(set.getString("tagname")));
     }
-    set.close();
-    closeConnections(connection, statement);
     return tags;
   }
 
@@ -160,13 +146,10 @@ public class DatabaseManager implements Listener {
     }
 
     try {
-      Connection connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM `usertags` WHERE uuid = ? AND tagname = ?;");
+      PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM `usertags` WHERE uuid = ? AND tagname = ?;");
       statement.setString(1, uuid.toString());
       statement.setString(2, tag.getName());
-      boolean hasPerm = statement.executeQuery().next();
-      closeConnections(connection, statement);
-      return hasPerm;
+      return statement.executeQuery().next();
     } catch (SQLException e) {
       e.printStackTrace();
       return false;
@@ -174,23 +157,18 @@ public class DatabaseManager implements Listener {
   }
 
   public void setActiveTag(UUID uuid, Tag tag) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("UPDATE `activetags` SET tagname = ? WHERE uuid = ?;");
+    PreparedStatement statement = getConnection().prepareStatement("UPDATE `activetags` SET tagname = ? WHERE uuid = ?;");
     statement.setString(1, tag.getName());
     statement.setString(2, uuid.toString());
     statement.executeUpdate();
     activeTags.put(uuid, tag);
-    closeConnections(connection, statement);
   }
 
   private boolean hasActiveTag(UUID uuid) {
     try {
-      Connection connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM `activetags` WHERE uuid = ?;");
+      PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM `activetags` WHERE uuid = ?;");
       statement.setString(1, uuid.toString());
-      boolean hasTag = statement.executeQuery().next();
-      closeConnections(connection, statement);
-      return hasTag;
+      return statement.executeQuery().next();
     } catch (SQLException e) {
       e.printStackTrace();
       return false;
@@ -198,25 +176,19 @@ public class DatabaseManager implements Listener {
   }
 
   private void createActiveTag(UUID uuid) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("INSERT INTO `activetags` (uuid, tagname) VALUES (?, ?);");
+    PreparedStatement statement = getConnection().prepareStatement("INSERT INTO `activetags` (uuid, tagname) VALUES (?, ?);");
     statement.setString(1, uuid.toString());
     statement.setString(2, "none");
     statement.executeUpdate();
-    closeConnections(connection, statement);
   }
 
   private Tag getActiveTag(UUID uuid) {
     try {
-      Connection connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM `activetags` WHERE uuid = ?;");
+      PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM `activetags` WHERE uuid = ?;");
       statement.setString(1, uuid.toString());
       ResultSet set = statement.executeQuery();
       set.next();
-      Tag tag = getTag(set.getString("tagname"));
-      set.close();
-      closeConnections(connection, statement);
-      return tag;
+      return getTag(set.getString("tagname"));
     } catch (SQLException e) {
       e.printStackTrace();
       return noneTag;
@@ -224,25 +196,19 @@ public class DatabaseManager implements Listener {
   }
 
   private HashMap<String, Tag> getTags() throws SQLException {
-    Connection connection = getConnection();
-    ResultSet set = connection.createStatement().executeQuery("SELECT * FROM `tags`;");
+    ResultSet set = getConnection().createStatement().executeQuery("SELECT * FROM `tags`;");
     HashMap<String, Tag> tags = new HashMap<>();
     while (set.next()) {
       tags.put(set.getString("name"), new Tag(set.getInt("id"), set.getString("name"), set.getInt("price"), set.getString("text")));
     }
-    set.close();
-    closeConnections(connection, null);
     return tags;
   }
 
   private boolean isTag(String tagName) {
     try {
-      Connection connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE `name` = ?;");
+      PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM `tags` WHERE `name` = ?;");
       statement.setString(1, tagName);
-      boolean isTag = statement.executeQuery().next();
-      closeConnections(connection, statement);
-      return isTag;
+      return statement.executeQuery().next();
     } catch (SQLException e) {
       e.printStackTrace();
       return false;
@@ -250,39 +216,23 @@ public class DatabaseManager implements Listener {
   }
 
   public void createTag(String name, String text, int price) throws SQLException {
-    Connection connection = getConnection();
-    PreparedStatement statement = connection.prepareStatement("INSERT INTO `tags` (`name`, `text`, `price`) VALUE (?, ?, ?);");
+    PreparedStatement statement = getConnection().prepareStatement("INSERT INTO `tags` (`name`, `text`, `price`) VALUE (?, ?, ?);");
     statement.setString(1, name);
     statement.setString(2, text);
     statement.setInt(3, price);
     statement.executeUpdate();
     cachedTags = getTags();
-    closeConnections(connection, statement);
   }
 
   private Tag getTag(String name) throws SQLException {
-    Connection connection = getConnection();
     PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM `tags` WHERE `name` = ?;");
     statement.setString(1, name);
     ResultSet set = statement.executeQuery();
     set.next();
-    Tag tag = new Tag(set.getInt("id"), set.getString("name"), set.getInt("price"), set.getString("text"));
-    set.close();
-    closeConnections(connection, statement);
-    return tag;
+    return new Tag(set.getInt("id"), set.getString("name"), set.getInt("price"), set.getString("text"));
   }
 
-  private Connection getConnection() throws SQLException {
+  public Connection getConnection() throws SQLException {
     return dataSource.getConnection();
-  }
-
-  @SneakyThrows
-  private void closeConnections(Connection connection, PreparedStatement statement) {
-    if (connection.isClosed()) {
-      connection.close();
-    }
-    if (statement != null && statement.isClosed()) {
-      statement.close();
-    }
   }
 }
