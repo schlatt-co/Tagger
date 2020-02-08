083da0d8c8e9c83c5fcc0d17476243b535fb898f
JRoy <joshroy126@gmail.com>
2020-02-07 20:44:35 -0500

    Add reclaim perk action for company tags

--- src/main/java/io/github/jroy/tagger/util/CompanyTagPerk.java
+++ src/main/java/io/github/jroy/tagger/util/CompanyTagPerk.java
@@ -4,13 +4,16 @@ import dev.tycho.stonks.api.event.CompanyJoinEvent;
 import dev.tycho.stonks.api.event.CompanyKickEvent;
 import dev.tycho.stonks.api.event.CompanyRenameEvent;
 import dev.tycho.stonks.api.perks.CompanyPerk;
+import dev.tycho.stonks.api.perks.CompanyPerkAction;
 import dev.tycho.stonks.model.core.Company;
 import dev.tycho.stonks.model.core.Member;
+import dev.tycho.stonks.model.core.Role;
 import io.github.jroy.tagger.events.TaggerTagUpdateEvent;
 import io.github.jroy.tagger.sql.DatabaseManager;
 import io.github.jroy.tagger.sql.Tag;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
@@ -22,9 +25,26 @@ public class CompanyTagPerk extends CompanyPerk implements Listener {
   private final DatabaseManager databaseManager;
 
   public CompanyTagPerk(Plugin plugin, DatabaseManager databaseManager) {
-    super(plugin, "Company Tag", Material.NAME_TAG, 300000, "This perk allows you and your company members to have",
+    super(plugin, "Company Tag", Material.NAME_TAG, 300000, new String[]{"This perk allows you and your company members to have",
         "a custom tag in front if your name. This tag cam be selected from",
-        "the /tags menu. This tag has your company name as the tag.");
+        "the /tags menu. This tag has your company name as the tag."}, new CompanyPerkAction("Reclaim Tag", Material.CARROT_ON_A_STICK, Role.Employee,
+        "Reclaims company tag if somehow you don't have it") {
+      @Override
+      public void onExecute(Company company, Player executor) {
+        try {
+          Tag tag = databaseManager.getCachedTags().get("company_" + company.pk);
+          if (databaseManager.hasPermission(executor.getUniqueId(), tag)) {
+            executor.sendMessage(Utils.format("You already have claimed this tag!"));
+            return;
+          }
+          databaseManager.awardTag(executor.getUniqueId(), tag);
+          executor.sendMessage(Utils.format("Successfully reclaimed tag! Use /tags to equip it!"));
+        } catch (SQLException e) {
+          executor.sendMessage(Utils.format("Error while reclaiming tag!"));
+          e.printStackTrace();
+        }
+      }
+    });
     this.databaseManager = databaseManager;
   }
 
