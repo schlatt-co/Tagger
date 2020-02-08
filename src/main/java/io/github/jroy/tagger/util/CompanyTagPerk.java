29f10fe8534595b9d905f0846c0faaf49b4de1b2
JRoy <joshroy126@gmail.com>
2020-02-07 20:29:42 -0500

    Skip null members while awarding tags

--- src/main/java/io/github/jroy/tagger/util/CompanyTagPerk.java
+++ src/main/java/io/github/jroy/tagger/util/CompanyTagPerk.java
@@ -6,8 +6,10 @@ import dev.tycho.stonks.api.event.CompanyRenameEvent;
 import dev.tycho.stonks.api.perks.CompanyPerk;
 import dev.tycho.stonks.model.core.Company;
 import dev.tycho.stonks.model.core.Member;
+import io.github.jroy.tagger.events.TaggerTagUpdateEvent;
 import io.github.jroy.tagger.sql.DatabaseManager;
 import io.github.jroy.tagger.sql.Tag;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -32,6 +34,9 @@ public class CompanyTagPerk extends CompanyPerk implements Listener {
       databaseManager.createTag("company_" + company.pk, fetchTag(company.name), 0);
       Tag tag = databaseManager.getCachedTags().get("company_" + company.pk);
       for (Member member : company.members) {
+        if (member == null) {
+          continue;
+        }
         databaseManager.awardTag(member.playerUUID, tag);
       }
     } catch (SQLException e) {
@@ -68,6 +73,7 @@ public class CompanyTagPerk extends CompanyPerk implements Listener {
         && databaseManager.getOwnedTags().get(event.getKickedPlayer().getUniqueId()).stream().anyMatch(p -> p.getName().equals("company_" + event.getCompany().pk))) {
       try {
         databaseManager.revokeTag(event.getKickedPlayer().getUniqueId(), databaseManager.getCachedTags().get("company_" + event.getCompany().pk));
+        Bukkit.getPluginManager().callEvent(new TaggerTagUpdateEvent(event.getKickedPlayer().getUniqueId(), databaseManager.getCachedTags().get("none")));
       } catch (SQLException e) {
         e.printStackTrace();
       }
